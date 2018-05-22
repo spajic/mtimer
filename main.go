c7f4ce8c7999995f51692b7da8bf8c257136cca4
Alexey Vasilyev <bestspajic@gmail.com>
2018-05-22 13:49:23 +0300

    Call git ls-files

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	cmd := exec.Command("git", "ls-files")
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println(fmt.Errorf("ERROR on git ls-files! %v", err))
+		return
+	}
+	fmt.Printf("Out is:\n%s", out)
 }
