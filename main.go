e60c45c7f1f1ff9c7d09d36dcd2a907fc4436d59
Alexey Vasilyev <bestspajic@gmail.com>
2018-05-22 14:14:46 +0300

    Get and set mtime

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
+	"time"
+
+	"github.com/djherbis/times"
 )
 
 func main() {
@@ -13,4 +18,18 @@ func main() {
 		return
 	}
 	fmt.Printf("Out is:\n%s", out)
+
+	t, err := times.Stat("main.go")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("main.go mtime: ", t.ModTime())
+
+	mtime := time.Date(2006, time.February, 1, 3, 4, 5, 0, time.UTC)
+	atime := mtime
+	if err := os.Chtimes("main.go", atime, mtime); err != nil {
+		log.Fatal(err)
+	}
 }
