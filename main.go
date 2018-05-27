77897374d05a879edd74a3e85e991ed8a36bcec1
Alexey Vasilyev <bestspajic@gmail.com>
2018-05-27 19:52:57 +0300

    Change paths a bit

--- main.go
+++ main.go
@@ -13,34 +13,40 @@ import (
 	"github.com/djherbis/times"
 )
 
-var path string
+var filespath string
+var timespath string
 var store bool
 var apply bool
 
 func init() {
-	flag.StringVar(&path, "path", "", "path to folder with files")
+	flag.StringVar(&filespath, "filespath", "", "path to folder with files")
+	flag.StringVar(&timespath, "timespath", "", "path to folder with mtimer.dat")
 	flag.BoolVar(&store, "store", false, "to store mtimes")
 	flag.BoolVar(&apply, "apply", false, "to apply stored mtimes from mtimer.dat")
 }
 
 func main() {
 	flag.Parse()
-	if path == "" {
-		fmt.Println("PATH NOT SPECIFIED! EXIT NOW!")
+	if filespath == "" {
+		fmt.Println("filespath NOT SPECIFIED! EXIT NOW!")
+		return
+	}
+	if timespath == "" {
+		fmt.Println("timespath NOT SPECIFIED! EXIT NOW!")
 		return
 	}
 	if store && !apply {
 		fmt.Println("STORE MODE")
-		fmt.Println("Working with path =", path)
-		cmd := exec.Command("find", ".", "-and", "-not", "-path", "./node_modules*", "-and", "-not", "-path", "./tmp*")
-		cmd.Dir = path
+		fmt.Println("Working with path =", filespath)
+		cmd := exec.Command("find", ".", "-not", "-path", "./node_modules*", "-and", "-not", "-path", "./tmp*")
+		cmd.Dir = filespath
 		files, err := cmd.Output()
 		if err != nil {
 			fmt.Println(fmt.Errorf("ERROR on list files. EXIT NOW, %v", err))
 			return
 		}
 
-		pathToMtimerDat := path + "/mtimer.dat"
+		pathToMtimerDat := timespath + "/mtimer.dat"
 		fmt.Println("Create file", pathToMtimerDat)
 		out, err := os.Create(pathToMtimerDat)
 		if err != nil {
@@ -52,7 +58,7 @@ func main() {
 		scanner := bufio.NewScanner(bytes.NewReader(files))
 		for scanner.Scan() {
 			fileName := strings.Replace(scanner.Text(), ".", "", 1)
-			t, err := times.Stat(path + fileName)
+			t, err := times.Stat(filespath + fileName)
 			if err != nil {
 				fmt.Println("WARNING: ", err.Error())
 				continue
@@ -66,8 +72,9 @@ func main() {
 		fmt.Println("FINISHED SUCCESSFULLY")
 	} else if apply {
 		fmt.Println("APPLY MODE")
-		pathToMtimerDat := path + "/mtimer.dat"
+		pathToMtimerDat := timespath + "/mtimer.dat"
 		fmt.Println("Applying mtimes from", pathToMtimerDat)
+		fmt.Println("Updating files in", filespath)
 		fileHandle, err := os.Open(pathToMtimerDat)
 		if err != nil {
 			fmt.Println("CANT OPEN FILE mtimer.dat! FINISH")
@@ -77,7 +84,7 @@ func main() {
 		fileScanner := bufio.NewScanner(fileHandle)
 		updatedCount := 0
 		for fileScanner.Scan() {
-			fileName := path + fileScanner.Text()
+			fileName := filespath + fileScanner.Text()
 			fileScanner.Scan()
 			fileMtimeText := fileScanner.Text()
 			fileMtime, err := time.Parse(time.RFC3339, fileMtimeText)
@@ -95,6 +102,6 @@ func main() {
 		fmt.Println("Updated mtimes of", updatedCount, "files")
 		fmt.Println("FINISHED SUCCESSFULLY")
 	} else {
-		fmt.Println("Use with -store or -apply flag")
+		fmt.Println("Use with ---store or --apply flag")
 	}
 }
